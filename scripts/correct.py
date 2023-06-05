a7cca18d154de03459181c4c57a53b78911f9f02
PurpleBlueAloeVera <135618831+PurpleBlueAloeVera@users.noreply.github.com>
2023-06-05 23:46:24 +0200

    Update correct.py

--- scripts/correct.py
+++ scripts/correct.py
@@ -13,7 +13,6 @@ import modules.scripts as scripts
 class AloeReferencer(scripts.Script):
     def __init__(self):
         self.enabled = False
-        self.last_checked_time = time.time()
 
     def title(self):
         return "Aloe Referencer"
@@ -35,43 +34,41 @@ class AloeReferencer(scripts.Script):
         self.enabled = not self.enabled
         return self.enabled
 
-    def check_new_images(self, directory):
-        current_time = time.time()
-        if current_time - self.last_checked_time >= 60:  # Check every minute
-            self.last_checked_time = current_time
-            images = os.listdir(directory)
-            images = [image for image in images if image.endswith(('.jpg', '.png', '.jpeg'))]
-            if len(images) > 0:
-                return images[-1]  # Return the latest image in the directory
+    def get_last_image(self, directory):
+        images = os.listdir(directory)
+        images = [image for image in images if image.endswith(('.jpg', '.png', '.jpeg'))]
+        if len(images) > 0:
+            return os.path.join(directory, images[-1])  # Return the latest image in the directory
         return None
 
     def run(self, directory, reference_image):
         weight = 0.5  # Default value
 
         if self.enabled:
-            new_image = self.check_new_images(directory)
-            if new_image:
-                # Convert reference_image to OpenCV format
-                reference_img = cv2.cvtColor(np.array(reference_image), cv2.COLOR_RGB2BGR)
-                # Load the new image
-                new_image_path = os.path.join(directory, new_image)
-                last_image = cv2.imread(new_image_path)
-                # Adjust the last image to match the reference image
-                adjusted_img = adjust_image_to_reference(last_image, reference_img)
-                # Convert back to PIL Image for further enhancements
-                adjusted_img_pil = Image.fromarray(cv2.cvtColor(adjusted_img, cv2.COLOR_BGR2RGB))
-                # Apply contrast and sharpness with weight
-                final_img = apply_contrast_and_sharpness(adjusted_img_pil, weight, weight)
-                # Save the final image
-                final_image_path = os.path.splitext(new_image_path)[0] + '_edit' + os.path.splitext(new_image_path)[1]
-                final_img.save(final_image_path)
-                # Convert final image to Gradio interface format
-                final_img = Image.open(final_image_path)
-                return final_img
-            else:
-                print("No new images found since the script was enabled.")
+            # Load reference image
+            reference_img = cv2.cvtColor(np.array(reference_image), cv2.COLOR_RGB2BGR)
+
+            # Get the last image in the directory
+            last_image_path = self.get_last_image(directory)
+            if not last_image_path:
+                print('No images found in the directory.')
+                return
+
+            # Load the last image
+            last_image = cv2.imread(last_image_path)
+
+            # Adjust the last image to match the reference image
+            adjusted_img = adjust_image_to_reference(last_image, reference_img)
+            # Convert back to PIL Image for further enhancements
+            adjusted_img_pil = Image.fromarray(cv2.cvtColor(adjusted_img, cv2.COLOR_BGR2RGB))
+            # Apply contrast and sharpness with weight
+            final_img = apply_contrast_and_sharpness(adjusted_img_pil, weight, weight)
+            # Save the final image
+            final_image_path = os.path.splitext(last_image_path)[0] + '_edit' + os.path.splitext(last_image_path)[1]
+            final_img.save(final_image_path)
+            # Convert final image to Gradio interface format
+            final_img = Image.open(final_image_path)
+            return final_img
         else:
             print("Script is currently disabled.")
-            self.last_checked_time = time.time()
-
         return None
